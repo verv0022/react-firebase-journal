dae21a6c36f974ea9ad408c9e0c48dc56d325b5e
Brandon <verv0022@algonquinlive.com>
2020-07-17 02:23:26 -0400

    Updates

---
+++ precache-manifest.a42c9477b046cc811362635a21002c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ac03ad087349f84d4ac61f10f0d1bc",
+    "url": "/react-firebase-journal/index.html"
+  },
+  {
+    "revision": "0bce1a19ba62465a26b7",
+    "url": "/react-firebase-journal/static/css/main.c0a1debe.chunk.css"
+  },
+  {
+    "revision": "c0b7f0123aff15363aad",
+    "url": "/react-firebase-journal/static/js/2.e529324b.chunk.js"
+  },
+  {
+    "revision": "67cff36aa6befea3490f87aa5792a53a",
+    "url": "/react-firebase-journal/static/js/2.e529324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bce1a19ba62465a26b7",
+    "url": "/react-firebase-journal/static/js/main.77108fdd.chunk.js"
+  },
+  {
+    "revision": "31a06210d2d5f4d6fcb1",
+    "url": "/react-firebase-journal/static/js/runtime-main.72cdd99a.js"
+  }
+]);
